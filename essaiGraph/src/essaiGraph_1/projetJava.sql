134079458e7b9402469abe1754eb41f0361b5f66
thgodfroy <theodorasafi@icloud.com>
2019-05-25 23:06:05 +0200

    BDD complète (normalement) avec bonnes foreignkeys

--- essaiGraph/src/essaiGraph_1/projetJava.sql
+++ essaiGraph/src/essaiGraph_1/projetJava.sql
@@ -2,211 +2,279 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 24 mai 2019 à 12:06
--- Version du serveur :  5.7.23
--- Version de PHP :  7.2.10
+-- Host: localhost:8889
+-- Generation Time: May 25, 2019 at 09:05 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `projetJava`
+-- Database: `projetJava`
 --
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ecole`
+-- Table structure for table `anneescolaire`
 --
 
-DROP TABLE IF EXISTS `ecole`;
-CREATE TABLE IF NOT EXISTS `ecole` (
+CREATE TABLE `anneescolaire` (
+  `Id_anneescolaire` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulletin`
+--
+
+CREATE TABLE `bulletin` (
+  `Id_bulletin` varchar(30) NOT NULL,
+  `Id_trimestre` varchar(30) NOT NULL,
+  `Id_inscrip` varchar(30) NOT NULL,
+  `appreciation` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classe`
+--
+
+CREATE TABLE `classe` (
+  `Id_classe` varchar(30) NOT NULL,
   `Id_ecole` varchar(30) NOT NULL,
-  `nom_ecole` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id_ecole`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+  `Id_niveau` varchar(30) NOT NULL,
+  `Id_anneescolaire` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `anneescolaire`
+-- Table structure for table `detailbulletin`
 --
 
-DROP TABLE IF EXISTS `anneescolaire`;
-CREATE TABLE IF NOT EXISTS `anneescolaire` (
-  `Id_anneescolaire` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id_anneescolaire`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `detailbulletin` (
+  `Id_detail` varchar(30) NOT NULL,
+  `Id_bulletin` varchar(30) NOT NULL,
+  `Id_enseignement` varchar(30) NOT NULL,
+  `appreciation` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `trimestre`
+-- Table structure for table `discipline`
 --
 
-DROP TABLE IF EXISTS `trimestre`;
-CREATE TABLE IF NOT EXISTS `trimestre` (
+CREATE TABLE `discipline` (
+  `Id_discipline` varchar(30) NOT NULL,
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ecole`
+--
+
+CREATE TABLE `ecole` (
+  `Id_ecole` varchar(30) NOT NULL,
+  `nom_ecole` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enseignement`
+--
+
+CREATE TABLE `enseignement` (
+  `Id_enseignement` varchar(30) NOT NULL,
+  `Id_classe` varchar(30) NOT NULL,
+  `Id_discipline` varchar(30) NOT NULL,
+  `Id_personne` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluation`
+--
+
+CREATE TABLE `evaluation` (
+  `Id_evaluation` varchar(30) NOT NULL,
+  `Id_detail` varchar(30) NOT NULL,
+  `note` int(10) NOT NULL,
+  `appreciation` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inscription`
+--
+
+CREATE TABLE `inscription` (
+  `Id_inscrip` varchar(30) NOT NULL,
+  `Id_personne` varchar(30) NOT NULL,
+  `Id_classe` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `niveau`
+--
+
+CREATE TABLE `niveau` (
+  `Id_niveau` varchar(30) NOT NULL,
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personne`
+--
+
+CREATE TABLE `personne` (
+  `Id_personne` varchar(30) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `prénom` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trimestre`
+--
+
+CREATE TABLE `trimestre` (
   `Id_trimestre` varchar(100) NOT NULL,
   `Numero` int(30) NOT NULL,
   `Debut` varchar(255) NOT NULL,
   `Fin` varchar(255) NOT NULL,
-  -- clé étrangère année scol
-  ALTER TABLE `trimestre` ADD FOREIGN KEY (`Id_anneescolaire`) REFERENCES `anneescolaire`(`Id_anneescolaire`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-  PRIMARY KEY (`Id_trimestre`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `Id_anneescolaire` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Structure de la table `niveau`
+-- Indexes for dumped tables
 --
 
-DROP TABLE IF EXISTS `niveau`;
-CREATE TABLE IF NOT EXISTS `niveau` (
-  `Id_niveau` varchar(30) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id_niveau`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+--
+-- Indexes for table `anneescolaire`
+--
+ALTER TABLE `anneescolaire`
+  ADD PRIMARY KEY (`Id_anneescolaire`);
 
 --
--- Structure de la table `classe`
+-- Indexes for table `bulletin`
 --
-
-DROP TABLE IF EXISTS `classe`;
-CREATE TABLE IF NOT EXISTS `classe` (
-  `Id_classe` varchar(30) NOT NULL,
-  --clé étrangère id_ecole
-  CONSTRAINT Id_ecole
-    FOREIGN KEY (ecole)
-    REFERENCES ecole(Id_ecole)
-  --clé étrangère id_niveau
-  CONSTRAINT Id_niveau
-    FOREIGN KEY (niveau)
-    REFERENCES niveau(Id_niveau)
-  --clé étrangère Id_anneescolaire
-  CONSTRAINT Id_anneescolaire
-    FOREIGN KEY (anneescolaire)
-    REFERENCES anneescolaire(Id_anneescolaire)
-  PRIMARY KEY (`Id_classe`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
+ALTER TABLE `bulletin`
+  ADD PRIMARY KEY (`Id_bulletin`);
 
 --
--- Structure de la table `discipline`
+-- Indexes for table `classe`
 --
-
-DROP TABLE IF EXISTS `discipline`;
-CREATE TABLE IF NOT EXISTS `discipline` (
-  `Id_discipline` varchar(30) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id_discipline`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
+ALTER TABLE `classe`
+  ADD PRIMARY KEY (`Id_classe`),
+  ADD KEY `Id_ecole` (`Id_ecole`),
+  ADD KEY `Id_niveau` (`Id_niveau`),
+  ADD KEY `Id_anneescolaire` (`Id_anneescolaire`);
 
 --
--- Structure de la table `personne`
+-- Indexes for table `detailbulletin`
 --
-
-DROP TABLE IF EXISTS `personne`;
-CREATE TABLE IF NOT EXISTS `personne` (
-  `Id_personne` varchar(30) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  `prénom` varchar(30) NOT NULL,
-  `type` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id_personne`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
+ALTER TABLE `detailbulletin`
+  ADD PRIMARY KEY (`Id_detail`);
 
 --
--- Structure de la table `enseignement`
+-- Indexes for table `discipline`
 --
-
-DROP TABLE IF EXISTS `enseignement`;
-CREATE TABLE IF NOT EXISTS `enseignement` (
-  `Id_enseignement` varchar(30) NOT NULL,
-  -- clé étrangère classe
-  CONSTRAINT Id_classe
-    FOREIGN KEY (classe)
-    REFERENCES classe(Id_classe)
-  -- clé étrangère discipline
-  CONSTRAINT Id_discipline
-    FOREIGN KEY (discipline)
-    REFERENCES discipline(Id_discipline)
-  -- clé étrangère personne
-    CONSTRAINT Id_personne
-      FOREIGN KEY (personne)
-      REFERENCES personne(Id_personne)
-  PRIMARY KEY (`Id_enseignement`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+ALTER TABLE `discipline`
+  ADD PRIMARY KEY (`Id_discipline`);
 
 --
--- Structure de la table `inscription`
+-- Indexes for table `ecole`
 --
-
-DROP TABLE IF EXISTS `inscription`;
-CREATE TABLE IF NOT EXISTS `inscription` (
-  `Id_inscrip` varchar(30) NOT NULL,
-  -- clé étrangère classe
-  CONSTRAINT Id_classe
-    FOREIGN KEY (classe)
-    REFERENCES classe(Id_classe)
-    -- clé étrangère personne
-  CONSTRAINT Id_personne
-    FOREIGN KEY (personne)
-    REFERENCES personne(Id_personne)
-  PRIMARY KEY (`Id_inscrip`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
+ALTER TABLE `ecole`
+  ADD PRIMARY KEY (`Id_ecole`);
 
 --
--- Structure de la table `bulletin`
+-- Indexes for table `enseignement`
 --
-
-DROP TABLE IF EXISTS `bulletin`;
-CREATE TABLE IF NOT EXISTS `bulletin` (
-  `Id_bulletin` varchar(30) NOT NULL,
-  `Id_trimestre` varchar(30) NOT NULL,
-  `Id_inscrip` varchar(30) NOT NULL,
-  `appreciation` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id_bulletin`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
+ALTER TABLE `enseignement`
+  ADD PRIMARY KEY (`Id_enseignement`),
+  ADD KEY `Id_classe` (`Id_classe`),
+  ADD KEY `Id_discipline` (`Id_discipline`),
+  ADD KEY `Id_personne` (`Id_personne`);
 
 --
--- Structure de la table `detailbulletin`
+-- Indexes for table `evaluation`
 --
-
-DROP TABLE IF EXISTS `detailbulletin`;
-CREATE TABLE IF NOT EXISTS `detailbulletin` (
-  `Id_detail` varchar(30) NOT NULL,
-  `Id_bulletin` varchar(30) NOT NULL,
-  `Id_enseignement` varchar(30) NOT NULL,
-  `appreciation` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id_detail`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
+ALTER TABLE `evaluation`
+  ADD PRIMARY KEY (`Id_evaluation`);
 
 --
--- Structure de la table `evaluation`
+-- Indexes for table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD PRIMARY KEY (`Id_inscrip`),
+  ADD KEY `Id_personne` (`Id_personne`),
+  ADD KEY `Id_classe` (`Id_classe`);
+
+--
+-- Indexes for table `niveau`
+--
+ALTER TABLE `niveau`
+  ADD PRIMARY KEY (`Id_niveau`);
+
+--
+-- Indexes for table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`Id_personne`);
+
+--
+-- Indexes for table `trimestre`
+--
+ALTER TABLE `trimestre`
+  ADD PRIMARY KEY (`Id_trimestre`),
+  ADD KEY `Id_anneescolaire` (`Id_anneescolaire`);
+
+--
+-- Constraints for dumped tables
 --
 
-DROP TABLE IF EXISTS `evaluation`;
-CREATE TABLE IF NOT EXISTS `evaluation` (
-  `Id_evaluation` varchar(30) NOT NULL,
-  `Id_detail` varchar(30) NOT NULL,
-  `note` int(10) NOT NULL,
-  `appreciation` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id_evaluation`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+--
+-- Constraints for table `classe`
+--
+ALTER TABLE `classe`
+  ADD CONSTRAINT `classe_ibfk_1` FOREIGN KEY (`Id_ecole`) REFERENCES `ecole` (`Id_ecole`),
+  ADD CONSTRAINT `classe_ibfk_2` FOREIGN KEY (`Id_niveau`) REFERENCES `niveau` (`Id_niveau`),
+  ADD CONSTRAINT `classe_ibfk_3` FOREIGN KEY (`Id_anneescolaire`) REFERENCES `anneescolaire` (`Id_anneescolaire`);
+
+--
+-- Constraints for table `enseignement`
+--
+ALTER TABLE `enseignement`
+  ADD CONSTRAINT `enseignement_ibfk_1` FOREIGN KEY (`Id_classe`) REFERENCES `classe` (`Id_classe`),
+  ADD CONSTRAINT `enseignement_ibfk_2` FOREIGN KEY (`Id_discipline`) REFERENCES `discipline` (`Id_discipline`),
+  ADD CONSTRAINT `enseignement_ibfk_3` FOREIGN KEY (`Id_personne`) REFERENCES `personne` (`Id_personne`);
+
+--
+-- Constraints for table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD CONSTRAINT `inscription_ibfk_1` FOREIGN KEY (`Id_classe`) REFERENCES `classe` (`Id_classe`),
+  ADD CONSTRAINT `inscription_ibfk_2` FOREIGN KEY (`Id_personne`) REFERENCES `personne` (`Id_personne`);
+
+--
+-- Constraints for table `trimestre`
+--
+ALTER TABLE `trimestre`
+  ADD CONSTRAINT `trimestre_ibfk_1` FOREIGN KEY (`Id_anneescolaire`) REFERENCES `anneescolaire` (`Id_anneescolaire`);
