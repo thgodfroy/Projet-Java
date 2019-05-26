6416630a72209882f341ff2aa3adf062f41db749
thgodfroy <theodorasafi@icloud.com>
2019-05-26 11:31:47 +0200

    Update Test.java
    ajout dans le main

--- essaiGraph/src/essaiGraph_1/Test.java
+++ essaiGraph/src/essaiGraph_1/Test.java
@@ -3,7 +3,8 @@ import javax.swing.JFrame;
 
 public class Test {
   public static void main(String[] args){
-	  
+
 	  Research fen = new Research();
-  }       
+    Nextpage next= new Nextpage();
+  }
 }
