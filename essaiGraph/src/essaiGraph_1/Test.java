119a379931bace53f1e202b64602e5969245bc10
luluup <lucie.sternberger@gmail.com>
2019-05-26 13:55:33 +0200

    test connexion

--- essaiGraph/src/essaiGraph_1/Test.java
+++ essaiGraph/src/essaiGraph_1/Test.java
@@ -1,10 +1,15 @@
 package essaiGraph_1;
+import java.sql.SQLException;
+
 import javax.swing.JFrame;
 
+
 public class Test {
-  public static void main(String[] args){
+  public static void main(String[] args) throws ClassNotFoundException, SQLException{
 
 	  Research fen = new Research();
     Nextpage next= new Nextpage();
+    Connexion testcon= new Connexion("projetjava", "root", "");
+    
   }
 }
