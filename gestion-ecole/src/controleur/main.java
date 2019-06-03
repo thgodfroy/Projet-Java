626295eff15ebc426240670f7a4918553537a66a
thgodfroy <47527999+thgodfroy@users.noreply.github.com>
2019-06-03 14:08:11 +0200

    Add files via upload

---
+++ gestion-ecole/src/controleur/main.java
@@ -0,0 +1,9 @@
+package controleur;
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ gestion-ecole/src/modele/eleve.java
@@ -0,0 +1,48 @@
+package modele;
+//https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/26830-
+//liez-vos-tables-avec-des-objets-java-le-pattern-dao
+public class eleve {
+	
+	//ID
+	private int id = 0;
+	//Nom de l'élève
+	private String nom = "";
+	//Prénom de l'élève
+	private String prenom = "";
+	 
+	//constructeur
+	public eleve(int id, String nom, String prenom) {
+	    this.id = id;
+	    this.nom = nom;
+	    this.prenom = prenom;
+	  }
+	
+	public eleve(){
+		
+	}
+	     
+	public int getId() {
+	    return id;
+	}
+
+	public void setId(int id) {
+	    this.id = id;
+	}
+
+	public String getNom() {
+	    return nom;
+	}
+
+	public void setNom(String nom) {
+	    this.nom = nom;
+	}
+
+	public String getPrenom() {
+	    return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+	    this.prenom = prenom;
+	}   
+
+}
