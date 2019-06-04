d33059615a2496b1ea32e0a0b4c96cae2618a95b
thgodfroy <theodorasafi@icloud.com>
2019-06-04 14:44:11 +0200

    chgmt main

--- gestion-ecole/src/controleur/main.java
+++ gestion-ecole/src/controleur/main.java
@@ -3,10 +3,16 @@ package controleur;
 import vue.fenetre;
 
 public class main {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
 		fenetre fen = new fenetre();
-		
+		String name = "projetjava";
+		String login="root";
+		String password="";
+
+		Connexion c = new Connexion (name, login, password);
+
 	}
 
 }
