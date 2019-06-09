33dcc990e045fd32b6be92b4071bfa64eeb39c31
thgodfroy <theodorasafi@icloud.com>
2019-06-09 15:48:28 +0200

    b

--- gestion-ecole/src/projetJava.sql
+++ gestion-ecole/src/projetJava.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 09, 2019 at 01:46 PM
+-- Generation Time: Jun 09, 2019 at 01:48 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -212,7 +212,8 @@ CREATE TABLE `professeur` (
   `Id_prof` varchar(30) NOT NULL,
   `nom` varchar(30) NOT NULL,
   `prénom` varchar(30) NOT NULL,
-  `id_personne` varchar(30) NOT NULL
+  `id_personne` varchar(30) NOT NULL,
+  `discipline` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
